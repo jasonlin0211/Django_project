61b4f1d7da7b5a91b40c64239591904a8bf49c4b
Jian-Yuan Lin <jasonlin0211@gatech.edu>
2023-03-06 12:56:21 -0800

    django can be launched

--- provision.sh
+++ provision.sh
@@ -10,15 +10,15 @@ sudo apt-get update
 sudo apt-get install tree
 
 # install mysql8
-#if ! [ -e /vagrant/mysql-apt-config_0.8.24-1_all.deb ]; then
-#  wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb
-#fi
+if ! [ -e /vagrant/mysql-apt-config_0.8.24-1_all.deb ]; then
+  wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb
+fi
 
-#sudo su -
-#sudo dpkg -i mysql-apt-config_0.8.24-1_all.deb
-#sudo DEBIAN_FRONTEND=noninteractivate 
-#sudo apt-get install -y mysql-server
-#sudo apt-get install -y libmysqlclient-dev
+sudo su -
+sudo dpkg -i mysql-apt-config_0.8.24-1_all.deb
+sudo DEBIAN_FRONTEND=noninteractivate
+sudo apt-get install -y mysql-server
+sudo apt-get install -y libmysqlclient-dev
 
 if [ ! -f "/usr/bin/pip" ]; then
   sudo apt-get install -y python3-pip
@@ -34,7 +34,7 @@ pip install -U pip -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 sudo mysql -u root << EOF
-  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourpassword';
+  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'tel26338612';
   flush privileges;
   show databases;
   CREATE DATABASE IF NOT EXISTS twitter;
--- twitter/settings.py
+++ twitter/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'twitter',
+        'HOST': '0.0.0.0',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'tel26338612'
     }
 }
 
